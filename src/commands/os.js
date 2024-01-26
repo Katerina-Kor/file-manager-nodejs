f45f02925986f768a6d52841b85f1edd6f25a010
Katsiaryna Karabeika <katerina.korobeiko@gmail.com>
2024-01-26 23:15:10 +0400

    feat: add os commands

---
+++ src/commands/os.js
@@ -0,0 +1,23 @@
+import os from 'os';
+
+export const osCommand = (param) => {
+  switch (param) {
+    case '--EOL':
+      console.log(os.EOL);
+      break;
+    case '--cpus':
+      console.log(os.cpus());
+      break;
+    case '--homedir':
+      console.log(os.homedir());
+      break;
+    case '--username':
+      console.log(os.userInfo().username);
+      break;
+    case '--architecture':
+      console.log(process.arch);
+      break;
+    default:
+      console.log('try again');
+  }
+}
